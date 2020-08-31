7a588ca7791379c08273686756dde7bfc771e8d3
edwardm <etmargallo@gmail.com>
2020-08-30 20:47:15 -0700

    Initial Commit
    Downloaded the lastest theme from www.ecore.design, in order to check into GitHub

---
+++ hello-elementor-child/scripts.js
@@ -0,0 +1,76 @@
+/*
+  ECORE Design
+  by Edward Margallo
+  contact@ecore.design
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	// toggle mobile nav icon
+	if (!!document.querySelector(".hfe-nav-menu-icon")) {
+
+		let yCoordinate;
+		let navIcon = document.querySelector(".hfe-nav-menu-icon");
+
+		window.onscroll = function () {
+			yCoordinate = window.scrollY; // Value of scroll Y in px
+			// console.log(yCoordinate);
+			navIconToggle();
+		};
+
+		var navIconToggle = function () {
+			// console.log(navIcon.getBoundingClientRect());
+			if (yCoordinate >= 100) {
+				navIcon.classList.remove('show');
+				navIcon.classList.add('hide');
+			} else {
+				navIcon.classList.remove('hide');
+				navIcon.classList.add('show');
+			}
+		}
+	}
+
+	// if home page
+	// just use the wordpress home class
+	if (document.body.classList.contains('home')) {
+		// do something
+	}
+
+	// check what page we are on
+	// to only execute scripts when needed
+	let url = window.location.href;
+
+	// bitnami wordpress root url hack
+	// quick href check for root
+	if (url.includes("localhost") || url.includes("127.")) {
+
+		let elements = document.querySelectorAll("a");
+		Array.prototype.forEach.call(elements, function (el, i) {
+
+			let pathname = el.getAttribute("href");
+
+			if (el.getAttribute("href") === "/") {
+				el.setAttribute("href", "/wordpress/");
+			} else if (!pathname.startsWith("/wordpress/") &&
+				!pathname.startsWith("#") &&
+				!pathname.startsWith("mailto") &&
+				!pathname.startsWith("https://") &&
+				!pathname.startsWith("http://") &&
+				!pathname.startsWith("//")
+			) {
+				el.setAttribute("href", "/wordpress" + pathname);
+			}
+		});
+	}
+
+
+	// if contact page
+	if (url.includes('/contact')) {
+		// do something
+	}
+
+	// if portfolio section
+	if (url.includes('/portfolio/')) {
+		// do something
+	}
+});
---
+++ hello-elementor-child/template-parts/404.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<main role="main"
+	style="background-image: url('https://www.ecore.design/wp-content/uploads/2020/03/ecore-bg-hex-down-center.svg'); background-position: bottom center; background-repeat: no-repeat; background-size: cover; min-height: 650px;">
+	<section>
+		<div class="elementor-container elementor-column-gap-default">
+			<div class="elementor-row">
+				<div style="margin: 50px auto 0 auto;">
+
+					<?php if ( apply_filters( 'hello_elementor_page_title', true ) ) : ?>
+
+					<h1 class="entry-title"
+						style="float: none; display: block; width: 100%; position: static; text-align: center;">
+						<?php esc_html_e( 'Oops, nothing here', 'hello-elementor' ); ?></h1>
+
+					<?php endif; ?>
+					<div class="page-content">
+						<p class="text-dark" style="text-align: center;">
+							<strong><?php esc_html_e( 'Like nothing was found at this location', 'hello-elementor' ); ?></strong>
+						</p>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+</main>
